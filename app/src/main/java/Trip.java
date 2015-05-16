d235edf3d4af318976315185e2b1d3542298ba95
RaymondCKK <kokkeong0209@gmail.com>
2015-05-16 21:26:45 +0800

    Add Trip class

---
+++ app/src/main/java/Trip.java
@@ -0,0 +1,35 @@
+/**
+ * Created by User on 16 05 2015.
+ */
+
+import java.util.Calendar;
+public class Trip {
+
+    private String destination;
+    private int time;
+    private Calendar date;
+    private int capacity;
+
+    public Trip(String destination,int time, Calendar date, int capacity){
+        this.destination=destination;
+        this.time=time;
+        this.date=date;
+        this.capacity=capacity;
+    }
+
+    private String getDestination(){
+        return destination;
+    }
+
+    private int getTime(){
+        return time;
+    }
+
+    private Calendar getDate(){
+        return date;
+    }
+
+    private int getCapacity(){
+        return capacity;
+    }
+}
