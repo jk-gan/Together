49397059517273a9e3bd06b0f0c21e99ef8712de
Jun Kai <kuhn96@gmail.com>
2015-05-30 20:03:09 +0800

    Added color to action bar

--- app/src/main/java/com/example/micky/together/MainActivity.java
+++ app/src/main/java/com/example/micky/together/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.micky.together;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,13 +13,10 @@ import com.parse.ParseUser;
 
 
 public class MainActivity extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     @Override
