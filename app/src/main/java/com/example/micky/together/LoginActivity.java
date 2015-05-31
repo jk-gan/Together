936fcb0016b6a4345d50784b19b12bc64e20bc46
Mike <z.mike0411@gmail.com>
2015-06-01 01:23:11 +0800

    Login validation done

--- app/src/main/java/com/example/micky/together/LoginActivity.java
+++ app/src/main/java/com/example/micky/together/LoginActivity.java
@@ -2,12 +2,15 @@ package com.example.micky.together;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
+import android.text.method.KeyListener;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.LogInCallback;
@@ -33,34 +36,85 @@ public class LoginActivity extends ActionBarActivity {
 
 
         loginButton.setOnClickListener(new View.OnClickListener() {
+            Toast toast;
+            int count = 0;
             @Override
             public void onClick(View v) {
+                //to track number of login attempts.
 
                 // get username input and password input
-                String username = usernameField.getText().toString();
+                final String username = usernameField.getText().toString();
                 String password = passwordField.getText().toString();
 
-                // doing login function in backend
-                ParseUser.logInInBackground(username, password, new LogInCallback() {
-                    public void done(ParseUser user, ParseException e) {
-                        if (user != null) {
-                            // Hooray! The user is logged in.
-                            // redirect to main page
-                            Toast toast;
-                            toast = Toast.makeText(getApplicationContext(), "Login Successfully", Toast.LENGTH_SHORT);
-                            toast.show();
-                            //get user id
-                            String objectId = user.getString("objectId");
+                if (count == 3){
 
-                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                        } else {
-                            // Signup failed. Look at the ParseException to see what happened.
-                            Toast toast;
-                            toast = Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT);
-                            toast.show();
+                    final KeyListener usernameListener;
+                    final KeyListener passwordListener;
+                    usernameListener = usernameField.getKeyListener();
+                    passwordListener = passwordField.getKeyListener();
+                    //if try login 3 time in a row.
+                    Toast.makeText(getApplicationContext(),"You have attempt 3 consecutive login." +
+                            "\nPlease try again some times.",Toast.LENGTH_SHORT).show();
+                    new CountDownTimer(30000, 1000) {
+
+                        TextView timer = (TextView)findViewById(R.id.timerTextView);
+                        public void onTick(long millisUntilFinished) {
+                            usernameField.setKeyListener(null);
+                            passwordField.setKeyListener(null);
+                            timer.setVisibility(View.VISIBLE);
+                            loginButton.setVisibility(View.INVISIBLE);
+                            timer.setText("Seconds Remaining: " + millisUntilFinished / 1000);
+                        }
+
+                        public void onFinish() {
+                            count = 0;
+                            loginButton.setVisibility(View.VISIBLE);
+                            timer.setVisibility(View.INVISIBLE);
+                            usernameField.setKeyListener(usernameListener);
+                            passwordField.setKeyListener(passwordListener);
+                        }
+                    }.start();
+
+                }
+                else{
+                    //check empty fields
+                    if (username.matches("") || password.matches("")){
+                        Toast.makeText(getApplicationContext(),"Please fill all empty fields",Toast.LENGTH_SHORT).show();
+                        count++;
+                    }
+                    else{
+                        //check username space
+                        for(int index = 0 ; index < username.length() ; index++){
+                            if (username.charAt(index) == ' '){
+                                Toast.makeText(getApplicationContext(),"Username cannot contains spaces",Toast.LENGTH_SHORT).show();
+                                count++;
+                                break;
+                            }
+                            else if (index == username.length()-1){
+
+                                // doing login function in backend
+                                ParseUser.logInInBackground(username, password, new LogInCallback() {
+
+                                    public void done(ParseUser user, ParseException e) {
+                                        if (user != null) {
+                                            // Hooray! The user is logged in.
+                                            // redirect to main page
+                                            toast = Toast.makeText(getApplicationContext(), "Login Successfully", Toast.LENGTH_SHORT);
+                                            toast.show();
+                                            count = 0;
+                                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                        } else {
+                                            // Signup failed. Look at the ParseException to see what happened.
+                                            toast = Toast.makeText(getApplicationContext(), "Wrong Username or Password", Toast.LENGTH_SHORT);
+                                            toast.show();
+                                            count++;
+                                        }
+                                    }
+                                });
+                            }
                         }
                     }
-                });
+                }
             }
         });
     }
